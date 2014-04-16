686c520399a579006189ec16b22e37a84d53276b
Bernard Bolter <b@bernardbolter.com>
2014-04-15 18:15:43 -0700

    reworked photo and about section into wordpress loop

--- dist/about.php
+++ dist/about.php
@@ -12,126 +12,91 @@ Template Name: About
 
 <div class="about_wrapper">
 	<div id="about_top_box" class="clearfix">
-		<img src="<?php echo bloginfo( 'template_url' ); ?>/gfx/erik_about_portrait.jpg" alt="Portrait of Erik Augustin Palm" />
-		<p class="about_pad">Erik Augustin Palm is a Swedish journalist, translator and copywriter currently based in San Francisco, California. He is published in the biggest newspapers and magazines in Sweden, and has written extensively about culture, media, the arts, lifestyle, travel, IT and society in Sweden, America and Japan - the three countries he mainly divides his time between. For years, Erik's focus has been to interview the cultural icons of our time - and to write long feature stories about the cultural phenomenon that affects our lives.</p>
-		<h4>Erik only takes on copywriting where there is no risk for conflict with his journalism.</h4>
-		<p>Erik's education background contains Media, Popular Culture Critique, Art History and Design Studies at <a href="http://www.sfsu.edu">San Francisco State University</a>, a Bachelor's Degree in Film Studies from <a href="http://www.su.se">Stockholm University</a>, Photography Studies at <a href="http://www.sfai.com">San Francisco Art Institute</a>, and Diplomas in Journalism and Writing from the <a href="http://www.jakobsbergsfolkhogskola.se">Swedish trade school Jakobsbergs Folkhögskola</a>.</p>
-		<p>Erik is covering culture, media, the arts and society from all over the US, and commutes from his current base of San Francisco, California, to Los Angeles, to report about the entertainment industry there. Over the years Erik has interviewed many of the heaviest names in Swedish, American and Japanese culture, society and media, for Sweden's biggest newspapers and magazines - as well as for some English language magazines. Erik has also contributed to national Swedish radio <a href="http://sverigesradio.se/p3">channel P3</a>, as a commentator.</p>
-	</div>
+
+		<?php if (have_posts()) : ?>
+		<?php while (have_posts()) : the_post(); ?>
+		
+		<?php $about_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+		<img src="<?php echo $about_image ?>" alt="Portrait of Erik Augustin Palm" />
+		<?php the_content(); ?>
+		
+		<?php endwhile; ?>
+		<?php endif; ?>	
+
+	</div> <!-- about_top_box -->
+
+
 	<div id="about_celebs" class="clearfix">
-		<div class="about_celeb_wrap">
-			<img src="<?php echo bloginfo( 'template_url' ); ?>/gfx/celebs/bryan_cranston.jpg" alt="photo with Bryan Cranston" />
-			<p>BRYAN CRANSTON</p>
-		</div>
-		<div class="about_celeb_wrap">
-			<img src="<?php echo bloginfo( 'template_url' ); ?>/gfx/celebs/mary_j_blige.jpg" alt="photo with Mary J Blige" />
-			<p>MARY J BLIGE</p>
-		</div>
-		<div class="about_celeb_wrap">
-			<img src="<?php echo bloginfo( 'template_url' ); ?>/gfx/celebs/michael_c_hall.jpg" alt="photo with Michael C Hall" />
-			<p>MICHAEL C HALL</p>
-		</div>
-		<div class="about_celeb_wrap">
-			<img src="<?php echo bloginfo( 'template_url' ); ?>/gfx/celebs/greta_gerwig.jpg" alt="photo with Greta Gerwig" />
-			<p>GRETA GERWIG</p>
-		</div>
-		<div class="about_celeb_wrap">
-			<img src="<?php echo bloginfo( 'template_url' ); ?>/gfx/celebs/matt_groening.jpg" alt="photo with Matt Groening" />
-			<p>MATT GROENING</p>
-		</div>
-		<div class="about_celeb_wrap">
-			<img src="<?php echo bloginfo( 'template_url' ); ?>/gfx/celebs/michelle_williams.jpg" alt="photo with Michelle Williams" />
-			<p>MICHELLE WILLIAMS</p>
-		</div>
-		<div class="about_celeb_wrap">
-			<img src="<?php echo bloginfo( 'template_url' ); ?>/gfx/celebs/snoop_dogg.jpg" alt="photo with Snoop Dogg" />
-			<p>SNOOP DOGG</p>
-		</div>
-		<div class="about_celeb_wrap">
-			<img src="<?php echo bloginfo( 'template_url' ); ?>/gfx/celebs/woody_harrelson.jpg" alt="photo with Woody Harrelson" />
-			<p>WOODY HARRELSON</p>
-		</div>
-	</div>
+
+		<?php $args = array( 'post_type' => 'chilling_with', 'posts_per_page' => -1, 'order' => 'ASC' ); ?>
+		<?php $celeb_loop = new WP_Query( $args ); ?>
+
+	 
+			<?php if ( $celeb_loop->have_posts() ) : ?>
+			<?php while ( $celeb_loop->have_posts() ) : $celeb_loop->the_post(); ?>
+
+			<div class="about_celeb_wrap">
+				<?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+				<img src="<?php echo $feat_image ?>" alt="photo chilling with <?php the_title() ?>" />
+				<p><?php the_title() ?></p>
+			</div>			
+
+	 		<?php endwhile; ?>
+		<?php endif; ?>
+ 
+		<?php wp_reset_query();  // Restore global post data stomped by the_post(). ?>
+
+	</div> <!-- about_celebs -->
+
 	<div id="about_mid_box" class="clearfix">
-		<p>Since 2008, Erik's main client as a journalist has been the prestigious and multiple award-winning Swedish morning newspaper <a href="http://www.svd.se">Svenska Dagbladet</a>. The newspaper has been published since 1884, has a circulation of 160 000 print copies and reaches approximately million readers daily, when both online and print readers are counted. Between 2007 and 2009, Erik was also a editorial staff, journalist and film critic at <a href="http://www.nojesguiden.se">Nöjesguiden</a>, Sweden's biggest free entertainment magazine.</p>
-		<p>Through Svenska Dagbladet, Erik is a holder of a five year Media Visa to the United States. He is a member of <a href="https://www.sjf.se/about">The Swedish Union of Journalists</a>, and is registered for Swedish f-tax (corporate tax) for his company Augustin Media (patented business name).</p>
-		<p>Among the scholarship foundations that Erik has received work and study grants from so far, are <a href="http://www.japanstiftelsen.se">Japanstiftelsen - The Scholarship Foundation for Studies of Japanese Society</a>, <a href="http://www.sjsf.se">The Scandinavia-Japan Sasakawa Foundation</a> and The Swedish Union of Journalists' scholarship for education and skill development.</p>
-	</div>
+
+		<?php $about_two_loop = new WP_Query( 'page_id=260' ); ?>
+
+	 
+			<?php if ( $about_two_loop->have_posts() ) : ?>
+			<?php while ( $about_two_loop->have_posts() ) : $about_two_loop->the_post(); ?>
+
+			<?php the_content(); ?>			
+
+	 		<?php endwhile; ?>
+			<?php endif; ?>
+ 
+		<?php wp_reset_query();  // Restore global post data stomped by the_post(). ?>
+
+	</div> <!-- about_mid_box -->
+
 	<div id="about_bot_box" class="clearfix">
-		<h4>To contact Erik, please fill in the form below, </h4>
-		<h4>or send an e-mail to: <a href="mailto:erik@augustinpalm.se">erik@augustinpalm.se</a></h4>
-	</div>
+
+		<?php $about_email_loop = new WP_Query( 'page_id=263' ); ?>
+
+			<?php if ( $about_email_loop->have_posts() ) : ?>
+			<?php while ( $about_email_loop->have_posts() ) : $about_email_loop->the_post(); ?>
+
+			<?php the_content(); ?>			
+
+	 		<?php endwhile; ?>
+			<?php endif; ?>
+ 
+		<?php wp_reset_query();  // Restore global post data stomped by the_post(). ?>
+
+	</div> <!-- about_bot_box -->
+
 	<div id="about_clients_box">
 		<p class="about_clients_header">CLIENTS <span class="about_header_client_small">(IN SELECTION)</span>:</p>
-		<p><a href="http://www.magasinetarena.se">Arena</a></p>
-		<p><a href="http://www.bang.se">Bang</a></p>
-		<p><a href="http://www.cafe.se">Café</a></p>
-		<p>Cinema</p>
-		<p><a href="http://www.dn.se">Dagens Nyheter</a></p>
-		<p><a href="http://www.dalademokraten.se">Dala-Demokraten</a></p>
-		<p><a href="http://www.damernasvarld.se">Damernas Värld</a></p>
-		<p><a href="http://www.effektmagasin.se">Effekt: Klimatmagasinet</a></p>
-		<p><a href="http://www.etc.se">ETC</a></p>
-		<p><a href="http://www.etonshirts.com">Eton of Sweden</a></p>
-		<p><a href="http://www.dt.se">Falu Kuriren</a></p>
-		<p><a href="http://www.fightermag.se">Fighter Magazine</a></p>
-		<p><a href="http://www.filmcentrum.se">Film & TV</a></p>
-		<p><a href="http://www.magasinetfilter.se">Filter</a></p>
-		<p><a href="http://www.flm.nu">FLM</a></p>
-		<p><a href="http://www.fokus.se">Fokus</a></p>
-		<p><a href="http://www.f12.se">Fredsgatan 12</a></p>
-		<p><a href="http://www.gaffa.se">Gaffa</a></p>
-		<p>Gidappa!</p>
-		<p><a href="http://www.gp.se">Göteborgs-Posten</a></p>
-		<p><a href="http://www.hd.se">Helsingborgs Dagblad</a></p>
-		<p><a href="http://www.hbl.fi">Hufvudstadsbladet</a></p>
-		<p><a href="http://www.iconmagazine.se">Icon Magazine</a></p>
-		<p>Ingmar</p>
-		<p><a href="http://www.jusektidningen.se">Jusektidningen</a></p>
-		<p><a href="http://www.kingmagazine.se">King Magazine</a></p>
-		<p><a href="http://www.kingsizemagazine.se">Kingsize</a></p>
-		<p><a href="http://www.konstvarlden.se">Konstvärlden</a></p>
-		<p><a href="http://www.loading.se">Level</a></p>
-		<p><a href="http://www.lira.se">Lira Musikmagasin</a></p>
-		<p><a href="http://www.magasinet-television.se">Magasinet Television</a></p>
-		<p><a href="http://www.metro.se">Metro Sweden</a></p>
-		<p><a href="http://modernpsykologi.wordpress.com">Modern Psykologi</a></p>
-		<p><a href="http://www.moviezine.se">MovieZine</a></p>
-		<p><a href="http://www.myspace.com">MySpace</a></p>
-		<p><a href="http://www.nyteknik.se">Ny Teknik</a></p>
-		<p><a href="http://www.nojesguiden.se">Nöjesguiden</a></p>
-		<p><a href="http://www.opusmagasin.se">Opus</a></p>
-		<p><a href="http://www.ottar.se">Ottar</a></p>
-		<p><a href="http://www.sverigesradio.se/p3">P3 Nyheter</a></p>
-		<p>People Sverige</p>
-		<p>Picture Talk</p>
-		<p><a href="http://www.plazamagazine.se">Plaza Magazine</a></p>
-		<p><a href="http://www.plazakvinna.com">Plaza Kvinna</a></p>
-		<p><a href="http://www.qx.se">QX</a></p>
-		<p>Release Print</p>
-		<p><a href="http://www.res.se">RES</a></p>
-		<p>Rocky Magasin</p>
-		<p><a href="http://www.redcross.se">Röda Korsets tidning Henry</a></p>
-		<p><a href="http://www.sats.se">SATS Magasin</a></p>
-		<p><a href="http://www.scanorama.com">Scanorama</a></p>
-		<p><a href="http://www.sf360.org">SF360</a></p>
-		<p><a href="http://www.sonicmagazine.com">Sonic Magazine</a></p>
-		<p><a href="http://www.svd.se">Svenska Dagbladet</a></p>
-		<p><a href="http://www.sydsvenskan.se">Sydsvenskan</a></p>
-		<p><a href="http://www.teatertidningen.se">Teatertidningen</a></p>
-		<p>Tidningen Om</p>
-		<p><a href="http://www.tidningenskriva.se">Tidningen Skriva</a></p>
-		<p><a href="http://www.unt.se">Upsala Nya Tidning</a></p>
-		<p><a href="http://www.vilaser.se">Vi Läser</a></p>
-		<p><a href="http://www.vice.com">Vice Magazine</a></p>
-	</div>
-</div>
 
-<?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>
-<?php the_content(); ?>
-<?php endwhile; ?>
-<?php endif; ?>
+		<?php $about_clients_loop = new WP_Query( 'page_id=266' ); ?>
+
+			<?php if ( $about_clients_loop->have_posts() ) : ?>
+			<?php while ( $about_clients_loop->have_posts() ) : $about_clients_loop->the_post(); ?>
+
+			<?php the_content(); ?>			
+
+	 		<?php endwhile; ?>
+			<?php endif; ?>
+ 
+		<?php wp_reset_query();  // Restore global post data stomped by the_post(). ?>
+
+	</div>
+</div> <!-- about_wrapper -->
 
 <?php get_footer(); ?>
-
--- dist/photography.php
+++ dist/photography.php
@@ -19,131 +19,31 @@ Template Name: Photo
 	</h1>
 	<?php endwhile; ?>
 	<?php endif; ?>
-</div>
+</div> <!-- photo_bio_box -->
 
 <div id="photo_loop_wrap">
 	<div id="photo_loop_container">
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/01_photo_big.jpg" class="swipebox" title="Temporary senior living in Fukushima City, Japan, October 2012">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/01_photo_sm.jpg" alt="Temporary senior living in Fukushima City">
-				<p>Temporary senior living in Fukushima City<span class="photo_date">Japan, October 2012</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/02_photo_big.jpg" class="swipebox" title="Soma, Fukushima, Japan, October 2012">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/02_photo_sm.jpg" alt="Soma" >
-				<p>Soma<span class="photo_date">Fukushima, Japan, October 2012</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/03_photo_big.jpg" class="swipebox"  title="Trumpet icon Kermit Ruffins outside his speakeasy, New Orleans, May 2013">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/03_photo_sm.jpg" alt="Trumpet icon Kermit Ruffins outside his speakeasy">
-				<p>Trumpet icon Kermit Ruffins outside his speakeasy<span class="photo_date">New Orleans, May 2013</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/04_photo_big.jpg" class="swipebox"  title="Trombone star Glen David Andrews in his backyard, New Orleans, May 2013">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/04_photo_sm.jpg" alt="Trombone star Glen David Andrews in his backyard">
-				<p>Trombone star Glen David Andrews in his backyard<span class="photo_date">New Orleans, May 2013</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/05_photo_big.jpg" class="swipebox"  title="Indie-punk band Nude Boy, San Diego, California, April 2010">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/05_photo_sm.jpg" alt="Indie-punk band Nude Boy">
-				<p>Indie-punk band Nude Boy<span class="photo_date">San Diego, California, April 2010</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/06_photo_big.jpg" class="swipebox"  title="San Diego, California, April 2010">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/06_photo_sm.jpg" alt="San Diego, California, April 2010">
-				<p>San Diego, California, April 2010<span class="photo_date">California, April 2010</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/07_photo_big.jpg" class="swipebox"  title="Abandoned house, Detroit, February 2012">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/07_photo_sm.jpg" alt="Abandoned house">
-				<p>Abandoned house<span class="photo_date">Detroit, February 2012</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/08_photo_big.jpg" class="swipebox"  title="Albaicin, Granada, Spain, November 2009">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/08_photo_sm.jpg" alt="Albaicin">
-				<p>Albaicin<span class="photo_date">Granada, Spain, November 2009</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/09_photo_big.jpg" class="swipebox"  title="Googleplex, Mountain View, California, June 2013">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/09_photo_sm.jpg" alt="Googleplex">
-				<p>Googleplex<span class="photo_date">Mountain View, California, June 2013</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/10_photo_big.jpg" class="swipebox"  title="Singer-songwriter icon Linda Perhacs, Topanga Canyon, California, April 2013">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/10_photo_sm.jpg" alt="Singer-songwriter icon Linda Perhacs">
-				<p>Singer-songwriter icon Linda Perhacs<span class="photo_date">Topanga Canyon, California, April 2013</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/11_photo_big.jpg" class="swipebox"  title="Ngong Ping 360, Hong Kong, January 2009">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/11_photo_sm.jpg" alt="Ngong Ping 360">
-				<p>Ngong Ping 360<span class="photo_date">Hong Kong, January 2009</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/12_photo_big.jpg" class="swipebox"  title="Tropic Cinema, Key West, Florida, May 2013">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/12_photo_sm.jpg" alt="Tropic Cinema">
-				<p>Tropic Cinema<span class="photo_date">Key West, Florida, May 2013</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/13_photo_big.jpg" class="swipebox"  title="Caffe Trieste, North Beach, San Francisco, February 2012">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/13_photo_sm.jpg" alt="Caffe Trieste">
-				<p>Caffe Trieste<span class="photo_date">North Beach, San Francisco, February 2012</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/14_photo_big.jpg" class="swipebox"  title="Eureka Valley, San Francisco, February 2010">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/14_photo_sm.jpg" alt="Eureka Valley">
-				<p>Eureka Valley<span class="photo_date">San Francisco, February 2010</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/15_photo_big.jpg" class="swipebox"  title="Rainbows and Dragons Hike Inn, Big Sur, California, May 2013">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/15_photo_sm.jpg" alt="Rainbows and Dragons Hike Inn">
-				<p>Rainbows and Dragons Hike Inn<span class="photo_date">Big Sur, California, May 2013</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/16_photo_big.jpg" class="swipebox"  title="Fruitvale Station director Ryan Coogler, Downtown Oakland, California, October 2013">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/16_photo_sm.jpg" alt="Fruitvale Station director Ryan Coogler">
-				<p>"Fruitvale Station" director Ryan Coogler<span class="photo_date">Downtown Oakland, California, October 2013</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/17_photo_big.jpg" class="swipebox"  title="Metro station, Tokyo, Japan, January 2006">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/17_photo_sm.jpg" alt="Metro station">
-				<p>Metro station<span class="photo_date">Tokyo, Japan, January 2006</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/18_photo_big.jpg" class="swipebox"  title="Tsukiji Fish Market, Tokyo, Japan, January 2006">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/18_photo_sm.jpg" alt="Tsukiji Fish Market">
-				<p>Tsukiji Fish Market<span class="photo_date">Tokyo, Japan, January 2006</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/19_photo_big.jpg" class="swipebox"  title="Kyoto, Japan, December 2008">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/19_photo_sm.jpg" alt="Kyoto">
-				<p>Kyoto<span class="photo_date">Japan, December 2008</span></p>
-			</a>
-	 	</div>
-	 	<div class="photo_box">
-			<a rel="palm_gallery" href="<?php bloginfo( 'template_url' ); ?>/gfx/photography/20_photo_big.jpg" class="swipebox"  title="Kyary Pamyu Pamyu live in Harajuku, Tokyo, Japan, October 2012">
-				<img src="<?php bloginfo( 'template_url' ); ?>/gfx/photography/20_photo_sm.jpg" alt="Kyary Pamyu Pamyu live in Harajuku">
-				<p>Kyary Pamyu Pamyu live in Harajuku<span class="photo_date">Tokyo, Japan, October 2012</span></p>
-			</a>
-	 	</div>
-	</div>
-</div>
+
+		<?php $args = array( 'post_type' => 'photographs', 'posts_per_page' => -1, 'order' => 'ASC' ); ?>
+		<?php $photo_loop = new WP_Query( $args ); ?>
+
+	 
+			<?php if ( $photo_loop->have_posts() ) : ?>
+			<?php while ( $photo_loop->have_posts() ) : $photo_loop->the_post(); ?>
+
+			<div class="photo_box">
+				<a rel="palm_gallery" href="<?php echo get_field('large_photograph'); ?>" class="swipebox" title="<?php the_title() ?>, <?php echo get_field('photo_place_and_date'); ?>">
+					<img src="<?php echo get_field('small_photograph'); ?>" alt="<?php the_title() ?>">
+					<p><?php the_title() ?><span class="photo_date"><?php echo get_field('photo_place_and_date'); ?></span></p>
+				</a>
+	 		</div> <!-- photo_box -->
+
+	 		<?php endwhile; ?>
+		<?php endif; ?>
+ 
+		<?php wp_reset_query();  // Restore global post data stomped by the_post(). ?>
+
+	</div> <!-- photo_loop_container -->
+</div> <!-- photo_loop_wrap -->
 
 <?php get_footer(); ?>
